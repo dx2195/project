1fe7d19a2c3863c84d18b232c59a20c4426be30b
wangmh <wangmh15@126.com>
2019-12-06 23:27:26 +0000

    Add map.html and sightings.html

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
   
 from . import views
+
 urlpatterns = [
-        path('sightings/', views.sightings),
+        path('sightings/', views.sightings, name = 'sightings'),
+        path('map/', views.map, name = 'map'),
         path('<unique-squirrel-id>/', views.sighting_details),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,17 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Squirrel
+from .forms import SquirrelForm
 
 def sightings(request):
-    text = ''
     sightings = Squirrel.objects.all()
     context = {
-        'sightings':sightings,
+        'sightings': sightings,
     }
     return render(request, 'tracker/sightings.html', context)
 
 def sighting_details(request, unique_squirrel_ID):
     squirrel = Squirrel.objects.get(id=unique_squirrel_ID)
     return HttpReponse(squirrel.unique_squirrel_ID)
+
+def map(request):
+    sightings = Squirrel.objects.all()[:100]
+    context = {
+        'sightings': sightings,
+    }
+    return render(request, 'tracker/map.html', context)
 # Create your views here.
