5d3f7037d288c5640a1c5e023c31f6ef4d1a7b78
wangmh <wangmh15@126.com>
2019-12-08 20:15:32 +0000

    Debug views.py

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Count
 
 from .models import Squirrel
 from .forms import SquirrelForm
