26ccb65efe108e5b6e0036f1368de4be55c54ffc
wangmh <wangmh15@126.com>
2019-12-08 19:35:36 +0000

    Debug urls.py

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
         path('sightings/', views.sightings, name = 'sightings'),
+        path('sightings/add/', views.create, name = 'create'),
         path('map/', views.map, name = 'map'),
-        path('sightings/<unique_squirrel_ID>', views.update, name = 'update'),
-        path('sightings/add', views.create, name = 'create'),
+        path('sightings/<unique_squirrel_ID>/', views.update, name = 'update'),
 ]
