344f49b7df721073a34c823f4675ad6b0dec0052
wangmh <wangmh15@126.com>
2019-12-08 05:58:19 +0000

    Modify models.py

--- tracker/models.py
+++ tracker/models.py
@@ -4,18 +4,18 @@ from django.utils.translation import gettext as _
 
 class Squirrel(models.Model):
     latitude = models.FloatField(
-        help_text=_('Latitude of squirrel sighting point'),
+        help_text = _('Latitude of squirrel sighting point'),
     )
 
     longitude = models.FloatField(
-        help_text=_('Longitude of squirrel sighting point'),
+        help_text = _('Longitude of squirrel sighting point'),
     )
 
     unique_squirrel_ID = models.CharField(
-        help_text=_('ID for each squirrel sighting'),
-        max_length=20,
-        unique=True,
-        primary_key=True,
+        help_text = _('ID for each squirrel sighting'),
+        max_length = 20,
+        unique = True,
+        primary_key = True,
     )
 
     AM = 'AM'
@@ -26,14 +26,14 @@ class Squirrel(models.Model):
     )
 
     shift = models.CharField(
-        help_text=_('Shift of the day'),
-        max_length=5,
-        choices=SHIFT_CHOICES,
-        default=AM,
+        help_text = _('Shift of the day'),
+        max_length = 5,
+        choices = SHIFT_CHOICES,
+        default = AM,
     )
 
     date = models.DateField(
-        help_text=_('Date of sighting'),
+        help_text = _('Date of sighting'),
     )
 
     ADULT = 'Adult'
@@ -47,10 +47,10 @@ class Squirrel(models.Model):
     )
 
     age = models.CharField(
-        help_text=_('Age of the squirrel'),
-        max_length=10,
-        choices=AGE_CHOICES,
-        default=UNKNOWN,
+        help_text = _('Age of the squirrel'),
+        max_length = 10,
+        choices = AGE_CHOICES,
+        default = UNKNOWN,
     )
 
     GRAY = 'Gray'
@@ -66,10 +66,10 @@ class Squirrel(models.Model):
     )
 
     primary_fur_color = models.CharField(
-        help_text=_('Primary fur color of the squirrel'),
-        max_length=10,
-        choices=COLOR_CHOICES,
-        default=UNKNOWN,
+        help_text = _('Primary fur color of the squirrel'),
+        max_length = 10,
+        choices = COLOR_CHOICES,
+        default = UNKNOWN,
     )
 
     GP = 'Ground Plane'
@@ -83,15 +83,16 @@ class Squirrel(models.Model):
     )
 
     location = models.CharField(
-        help_text=_('Location of the squirrel'),
-        max_length=20,
-        choices=LOCATION_CHOICES,
-        default=UNKNOWN,
+        help_text = _('Location of the squirrel'),
+        max_length = 20,
+        choices = LOCATION_CHOICES,
+        default = UNKNOWN,
     )
 
     specific_location = models.CharField(
-        help_text=_('Specific location of the squirrel'),
-        max_length=200,
+        help_text = _('Specific location of the squirrel'),
+        max_length = 200,
+        blank = True,
     )
 
     T = 'TRUE'
@@ -103,99 +104,100 @@ class Squirrel(models.Model):
     )
 
     running = models.CharField(
-        help_text=_('Whether the squirrel was seen running'),
-        max_length=10,
-        choices=TF_CHOICES,
-        default=F,
+        help_text = _('Whether the squirrel was seen running'),
+        max_length = 10,
+        choices = TF_CHOICES,
+        default = F,
     )
 
     chasing = models.CharField(
-        help_text=_('Whether the squirrel was seen chasing another squirrel'),
-        max_length=10,
-        choices=TF_CHOICES,
-        default=F,
+        help_text = _('Whether the squirrel was seen chasing another squirrel'),
+        max_length = 10,
+        choices = TF_CHOICES,
+        default = F,
     )
 
     climbing = models.CharField(
-        help_text=_('Whether the squirrel was seen climbing'),
-        max_length=10,
-        choices=TF_CHOICES,
-        default=F,
+        help_text = _('Whether the squirrel was seen climbing'),
+        max_length = 10,
+        choices = TF_CHOICES,
+        default = F,
     )
 
     eating = models.CharField(
-        help_text=_('Whether the squirrel was seen eating'),
-        max_length=10,
-        choices=TF_CHOICES,
-        default=F,
+        help_text = _('Whether the squirrel was seen eating'),
+        max_length = 10,
+        choices = TF_CHOICES,
+        default = F,
     )
 
     foraging = models.CharField(
-        help_text=_('Whether the squirrel was seen foraging for food'),
-        max_length=10,
-        choices=TF_CHOICES,
-        default=F,
+        help_text = _('Whether the squirrel was seen foraging for food'),
+        max_length = 10,
+        choices = TF_CHOICES,
+        default = F,
     )
 
     other_activities = models.CharField(
-        help_text=_('Other activities of the squirrel'),
-        max_length=200,
+        help_text = _('Other activities of the squirrel'),
+        max_length = 200,
+        blank = True,
     )
 
     kuks = models.CharField(
-        help_text=_('Whether the squirrel was heard kukking'),
-        max_length=10,
-        choices=TF_CHOICES,
-        default=F,
+        help_text = _('Whether the squirrel was heard kukking'),
+        max_length = 10,
+        choices = TF_CHOICES,
+        default = F,
     )
 
     quaas = models.CharField(
-        help_text=_('Whether the squirrel was heard quaaing'),
-        max_length=10,
-        choices=TF_CHOICES,
-        default=F,
+        help_text = _('Whether the squirrel was heard quaaing'),
+        max_length = 10,
+        choices = TF_CHOICES,
+        default = F,
     )
 
     moans = models.CharField(
-        help_text=_('Whether the squirrel was heard moaning'),
-        max_length=10,
-        choices=TF_CHOICES,
-        default=F,
+        help_text = _('Whether the squirrel was heard moaning'),
+        max_length = 10,
+        choices = TF_CHOICES,
+        default = F,
     )
 
     tail_flags = models.CharField(
-        help_text=_('Whether the squirrel was seen flagging tail'),
-        max_length=10,
-        choices=TF_CHOICES,
-        default=F,
+        help_text = _('Whether the squirrel was seen flagging tail'),
+        max_length = 10,
+        choices = TF_CHOICES,
+        default = F,
     )
 
     tail_twitches = models.CharField(
-        help_text=_('Whether the squirrel was seen twitching tail'),
-        max_length=10,
-        choices=TF_CHOICES,
-        default=F,
+        help_text = _('Whether the squirrel was seen twitching tail'),
+        max_length = 10,
+        choices = TF_CHOICES,
+        default = F,
     )
 
     approaches = models.CharField(
-        help_text=_('Whether the squirrel was seen approaching human'),
-        max_length=10,
-        choices=TF_CHOICES,
-        default=F,
+        help_text = _('Whether the squirrel was seen approaching human'),
+        max_length = 10,
+        choices = TF_CHOICES,
+        default = F,
     )
 
     indifferent = models.CharField(
-        help_text=_('Whether the squirrel was indifferent to human'),
-        max_length=10,
-        choices=TF_CHOICES,
-        default=F,
+        help_text = _('Whether the squirrel was indifferent to human'),
+        max_length = 10,
+        choices = TF_CHOICES,
+        default = F,
     )
 
     runs_from = models.CharField(
-        help_text=_('Whether the squirrel was seen running from human'),
-        max_length=10,
-        choices=TF_CHOICES,
-        default=F,
+        help_text = _('Whether the squirrel was seen running from human'),
+        max_length = 10,
+        choices = TF_CHOICES,
+        default = F,
     )
 
     def __str__(self):
