bbaadcdb8eb5e83c0c985958b93efc6a4aff6c82
wangmh <wangmh15@126.com>
2019-12-08 20:05:13 +0000

    Add stats view to urls.py

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
         path('sightings/', views.sightings, name = 'sightings'),
         path('sightings/add/', views.create, name = 'create'),
+        path('sightings/stats/', views.stats, name = 'stats'),
         path('map/', views.map, name = 'map'),
         path('sightings/<unique_squirrel_ID>/', views.update, name = 'update'),
 ]
